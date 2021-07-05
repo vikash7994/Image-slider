9bc7c773f14c3f41580c915ffe1a3cd9df5e82e5
vikas gupta <76902305+vikash7994@users.noreply.github.com>
2021-07-05 19:02:02 +0530

    Update main.js

--- image_slider/js/main.js
+++ image_slider/js/main.js
@@ -1,51 +1,26 @@
-jQuery(document).ready(function ($) {
 
-  //variables
-  var slideCount = $('#slider ul li').length;
-  var slideWidth = $('#slider ul li').width();
-  var slideHeight = $('#slider ul li').height();
-  var sliderUlWidth = slideCount * slideWidth;
 
- $('#slider').css({ width: slideWidth, height: slideHeight });
+	$(document).ready(function(){
+		var x = 0;
+		$('.btn-next').click(function(){
+        x= (x<=300)?(x+100):0;
+      $('figure').css('left', -x+'%');
+    
+		});
 
-  $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
 
-  $('#slider ul li:last-child').prependTo('#slider ul');
-
-  function moveLeft() {
-      $('#slider ul').animate({
-          left: + slideWidth
-      }, 300,
-       function () {
-          $('#slider ul li:last-child').prependTo('#slider ul');
-          $('#slider ul').css('left', '');
+     // for prev slide
+     $('.btn-prev').click(function(){
+        x= (x>=100)?(x-100):400;
+      $('figure').css('left', -x+'%');
+    });
+	});
+  document.addEventListener('DOMContentLoaded',init);
+  function init(){
+      let query = window.matchMedia("(max-width:768px)");
+      if(query.matches){
+          document.querySelector('div#slider figure img:last-child').style.visibility ="hidden";
       }
-      );
-  };
+ 
 
-  function moveRight() {
-      $('#slider ul').animate({
-          left: - slideWidth
-      }, 300, function () {
-          $('#slider ul li:first-child').appendTo('#slider ul');
-          $('#slider ul').css('left', '');
-      });
-  };
-
-  $('.control_prev').click(function () {
-      moveLeft();
-  });
-
-  $('.control_next').click(function () {
-      moveRight();
-  });
-
-});
-
-document.addEventListener('DOMContentLoaded',init);
-function init(){
-    let query = window.matchMedia("(max-width:768px)");
-    if(query.matches){
-        document.querySelector('#slider ul li:last-child').style.display ="none";
-    }    
-}
+    }
